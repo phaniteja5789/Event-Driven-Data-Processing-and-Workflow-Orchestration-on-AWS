aea88f0f3b3701e4ff269c52e4f128db495e945f
phaniteja5789 <krishphaniteja9@gmail.com>
2023-10-28 18:10:28 +0530

    Initial commit with all code files

---
+++ StockDataETLJOB.py
@@ -0,0 +1,27 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# Script generated for node Amazon S3
+AmazonS3_node1698490179840 = glueContext.create_dynamic_frame.from_options(
+    format_options={"multiline": False},
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "paths": ["s3://stockdatabucket2810202312/StockData"],
+        "recurse": True,
+    },
+    transformation_ctx="AmazonS3_node1698490179840",
+)
+
+job.commit()
